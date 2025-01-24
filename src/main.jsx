b83e8bc013c766f1924c7f719b99ea9741e37195
pixelPicker <rj568614gmail.com>
2025-01-24 18:47:07 +0530

    added a simple router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, createBrowserRouter } from 'react-router'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'blog', element: <Blog /> },
+      { path: 'projects', element: <Projects /> },
+      { path: 'case studies', element: <CaseStudies /> },
+      { path: '*', element: <Page404 /> },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
