5addf74d96473522c4feb8fc30a9029865010d63
pixelPicker <rj568614gmail.com>
2025-01-25 17:08:13 +0530

    created CTA and Info section

--- src/HomePageComponents/Info.jsx
+++ src/HomePageComponents/Info.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { FcGlobe } from "react-icons/fc";
+import { FaLeaf } from "react-icons/fa6";
+import { Link } from "react-router";
+import { MdArrowOutward } from "react-icons/md";
 
 function Info() {
   return (
-    <section>
-      
+    <section className=" bg-gray-200 p-16 flex flex-col gap-16">
+      <div className="w-2/3">
+        <h1 className="font-MontserratBold text-4xl text-gray-700 pb-8">
+          Imagine. Act. Transform. Join Us in Building a Greener Tomorrow!
+        </h1>
+        <div>
+          <Link className="bg-green-500 hover:bg-green-400 active:bg-green-300 font-Nunito px-4 py-3 rounded-full transition-all">Get Invloved Today
+          </Link>
+        </div>
+      </div>
+
     </section>
   );
 }
--- src/ProjectComponents/Page404.jsx
+++ src/ProjectComponents/Page404.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router";
 function Page404() {
   return (
     <div className="grid place-items-center min-h-screen">
-      <div className="grid justify-center gap-2">
-        <h1 className="font-Montserrat text-8xl text-center">404</h1>
-        <h1 className="font-Montserrat text-2xl text-center">Page Not Found</h1>
+      <div className="grid justify-items-center gap-2">
+        <img src="/assets/images/cat_pic_1.jpg" className="aspect-9/16 w-1/1"></img>
+        <h1 className="font-Montserrat text-2xl text-center">Couldn't find the page you asked</h1>
         <Link className="bg-black font-Montserrat text-white px-4 py-2 text-center rounded-3xl mx-4" to='/'>Go back home</Link>
       </div>
     </div>
