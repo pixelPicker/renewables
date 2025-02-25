931a7f9bab7d3afc67836406f31fea04fde65bef
Carlton-tech <coelhojoseph94@gmail.com>
2025-02-25 21:46:36 +0530

    Commit issue solve

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,8 @@ import AboutUs from './QuizComponent/About/AboutUs.jsx'
 import Quiz from './QuizComponent/Quiz/Quiz.jsx'
 import UpcomingEvents from './Upcoming/Upcoming.jsx'
 import CaseStudies from './Casestudy/Casestudy.jsx'
-<<<<<<< HEAD
 import Blog from './blog/blog.jsx'
-=======
 import Projects from './DIYProject/Projects.jsx'
->>>>>>> 886f9c684a4acb32c80749b58cb62e39552b2748
 const reactRouter = createBrowserRouter([
   {
     path: '/',
@@ -21,18 +18,11 @@ const reactRouter = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       //! Uncomment this only when you implement the pages
-<<<<<<< HEAD
       { path: 'blog', element: <Blog /> },
-      // { path: 'projects', element: <Projects /> },
-      { path: 'casestudy', element: <CaseStudies /> },
-{path:"upcoming",element:<UpcomingEvents/>},
-=======
-      // { path: 'blog', element: <Blog /> },
       { path: 'projects', element: <Projects /> },
       { path: 'casestudy', element: <CaseStudies /> },
   {path:"upcoming",element:<UpcomingEvents/>},
 
->>>>>>> 886f9c684a4acb32c80749b58cb62e39552b2748
       {path:'about' ,element:<AboutUs/>},
       {path:'quiz',element:<Quiz/>}
     ]
